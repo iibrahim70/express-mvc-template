dfcb67cbcc760e9e97eeee121c3d237a0bd8204f
iibrahim70 <aviknuhash70@gmail.com>
2024-03-02 19:36:50 +0600

    update readme env sample

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,7 +2,7 @@ import { Router } from 'express';
 
 const router = Router();
 
-const routes = [{ path: '/pathname', route: ImportRoute }];
+const routes = [{ path: '/routesPath', route: ExampleRoute }];
 
 routes.forEach((route) => router.use(route.path, route.route));
 
